af0ae63a330121a7a5a1d5cb398fc3d63ff36a27
Antony J Malakkaran <antonyjm462@gmail.com>
2020-05-20 21:42:59 +0530

    Folder tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { AuthComponent } from './auth/services/auth/auth.component';
+import { CoreComponent } from './core/components/core/core.component';
+import { ListComponent } from './circle/components/list/list.component';
+import { ItemComponent } from './circle/components/item/item.component';
+import { GroupComponent } from './circle/components/group/group.component';
+import { OwnerComponent } from './help/components/owner/owner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    AuthComponent,
+    CoreComponent,
+    ListComponent,
+    ItemComponent,
+    GroupComponent,
+    OwnerComponent,
   ],
   imports: [
     BrowserModule,
